7475200fd5b4a59e4fc3cddee643d538bf87fee0
swkim12345 <swkim12345@naver.com>
2025-08-08 11:42:18 +0900

    이분탐색 lower / upperbound 추가

---
+++ base/java/algorithm/lowerbound.java
@@ -0,0 +1,27 @@
+package base.java.algorithm;
+
+/*
+ * lowerbound
+ * 정렬된 값에서 입력값을 기준으로 입력값보다 크거나 같으면서 가장 작은 idx를 반환하는 함수
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class lowerbound {
+    static int lowerbound(List<Integer> list, int value) {
+        int max = list.size();
+        int min = 0;
+
+        while (min < max) {
+            int mid = (min + max) / 2;
+
+            if (value > list.get(mid)) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+        return min;
+    }
+}
---
+++ base/java/algorithm/upperbound.java
@@ -0,0 +1,30 @@
+package base.java.algorithm;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * upperbound
+ * 구하는 값(value)보다 큰 값이 처음 나오는 위치를 반환
+ * 만약, value와 같은 중복값 중 가장 마지막 값을 반환하려면 -1해서 반환해야 함.
+ */
+
+public class upperbound {
+    static int upperbound(List<Integer> list, int value) {
+        int max = list.size();
+        int min = 0;
+
+        while (min < max) {
+            int mid = (min + max) / 2;
+
+            if (value < list.get(mid)) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return min;
+        // 만약, value와 같은 중복값 중 가장 마지막 반환하려면?
+        // return min - 1;
+    }
+}
