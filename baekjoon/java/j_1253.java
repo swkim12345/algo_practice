5dc8346e59b04e2d274ea33b7ee1b341d44de666
swkim12345 <swkim12345@naver.com>
2025-08-12 11:18:46 +0900

    1253 update

--- baekjoon/java/j_1253.java
+++ baekjoon/java/j_1253.java
@@ -3,10 +3,16 @@
  * 
  * 문제 해설
  * 두개의 합 - 다른 수 => 좋다 좋다 개수 카운팅
+ * 현재 값은 같지만, 다른 인덱스라면 다른 수로 취급
+ * 수의 개수는 2000개, 수는 정수 -10억 ~ +10억까지
+ * 좋은 수 = 다른 두 수의 합으로 나타낼 수 있다면 좋은 수, 다른 인덱스라면 같은 값이여도 다른 수 취급
  * 
  * 문제 풀이
- * 인덱스 기준으로 2개 브루트포스
- * 좋은 수 판별 - HashSet 이용해서 있으면 좋은 수 카운팅 ++
+ * 값을 하나하나 검증, 검증할 두 수는 투포인터를 이용해 검사. start < end일 때 작동
+ * 다만, 대상 값의 인덱스와 투포인터의 인덱스가 동일하다면 안 됨(다른 수 두개의 합으로 나타내야 하기 때문)
+ * 
+ * 오답노트
+ * hashmap으로 풀려고 했음. 굳이.... 이렇게 갈 필요가 없었습니다...!
  */
 
 import java.io.*;
@@ -16,55 +22,46 @@ public class j_1253 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] arr;
 
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        List<Integer> list = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
-        int tmp;
-
+        
         for (int i = 0; i < N; i++) {
-            tmp = Integer.parseInt(st.nextToken());
-            arr[i] = tmp;
-            if (map.containsKey(tmp)) {
-                map.put(tmp, map.get(tmp) + 1);
-            } else {
-                map.put(tmp, 1);
-            }
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        int ans = 0, val;
+        list.sort((o1, o2) -> Integer.compare(o1, o2));
 
-        // 0이 3개 초과
-        if (map.containsKey(0)){
-            val = map.get(0);
-            if (val > 2) {
-                ans += val;
-                map.remove(0);
-            }
-        }
-
-
+        // 타깃값 결정
+        int target, p1, p2, tmp, ans = 0;
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                tmp = arr[i] + arr[j];
-                if (map.containsKey(tmp)) {
-                    val = map.get(tmp);
-                    // 만약 (3, 1) / (3, 2) 라면 (3이 동일, 다만 인덱스가 1, 2임)
-                    if (val == 1 && (arr[i] == tmp || arr[j] == tmp)) continue;
-                    if (val == 2 && arr[i] == tmp && arr[j] == tmp) continue;
-                    ans ++;
-                    
-                    if (val == 1) {
-                        map.remove(tmp);
-                    } else {
-                        map.put(tmp, val - 1);
-                    }
+            target = list.get(i);
+            p1 = 0; p2 = N - 1;
+            
+            while (p1 < p2) {
+                // 인덱스와 같은 포인터 인덱스 - continue
+                if (p1 == i) {
+                    p1++;
+                    continue;
+                }
+                if (p2 == i) {
+                    p2--;
+                    continue;
+                }
+                // 합이 같아면 ans 추가
+                tmp = list.get(p1) + list.get(p2);
+                if (tmp == target) {
+                    ans++;
+                    p1++; p2--;
+                    break;
+                } else if (tmp < target) {
+                    p1++;
+                } else {
+                    p2--;
                 }
             }
         }
-
         System.out.println(ans);
     }
 }
